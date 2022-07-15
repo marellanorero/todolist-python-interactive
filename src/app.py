15242c6b6d926e35d2bcc80e7147767d71218c7d
marellanorero <marellanonorero@gmail.com>
2022-07-15 02:22:27 +0000

    commit

---
+++ src/app.py
@@ -0,0 +1,30 @@
+import json
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+@app.route('/todos', methods=['GET'])
+def hello_world():
+    return jsonify(todos)
+
+
+todos = [
+    { "label": "My first task", "done": False },
+    { "label": "My second task", "done": False }
+]
+
+
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    request_body = request.data
+    decoded_object = json.loads(request_body)
+    print("Incoming request with the following body", decoded_object)
+    return 'Response for the POST todo'
+
+
+
+
+
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=3245, debug=True)
